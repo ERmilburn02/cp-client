33ed96032f4abc0d814712af7cb544b3691f29f2
Eliza Milburn <61363431+ERmilburn02@users.noreply.github.com>
2025-06-04 16:09:33 +0100

    Remove access to Electron API (#2)
    * Remove access to Electron API
    
    * Update Version UI in case `cpClientApi` isn't available

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,14 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
 
+interface CustomAPI {
+  versions: {
+    [key: string]: string | undefined
+  }
+}
+
 declare global {
   interface Window {
     electron: ElectronAPI
-    api: unknown
+    cpClientApi: CustomAPI
   }
 }
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,18 +1,17 @@
 import { useState } from 'react'
-
 import { Badge } from './ui/badge'
 
 function Versions(): JSX.Element {
-  const [versions] = useState(window.electron.process.versions)
+  const [versions] = useState(window.cpClientApi?.versions ?? {})
   const [flashVersion] = useState<string | null>(
     navigator.plugins['Shockwave Flash']?.description.substring('Shockwave Flash '.length) || null
   )
 
   return (
     <>
-      <Badge>Electron v{versions.electron}</Badge>
-      <Badge>Chromium v{versions.chrome}</Badge>
-      <Badge>Node v{versions.node}</Badge>
+      <Badge>Electron v{versions.electron || 'Unknown'}</Badge>
+      <Badge>Chromium v{versions.chrome || 'Unknown'}</Badge>
+      <Badge>Node v{versions.node || 'Unknown'}</Badge>
       {flashVersion && <Badge>Flash v{flashVersion}</Badge>}
     </>
   )
